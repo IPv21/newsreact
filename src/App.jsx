7f6bd10cf800abf0892a02f41fb9dd1cf1d9037d
ipv21 <willsherman771@gmail.com>
2024-11-09 08:57:15 -0500

    first push failed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import News from './components/News'
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <News />
+    </div>
+  )
+}
+
+export default App
